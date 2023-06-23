06981dee77e3cb0a7c52ad62408abb2ce66adf13
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 19:40:49 +0530

    [PALLAVI] Add: File

---
+++ UserRegistrationProblem/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace UserRegistrationProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("User Registration Problem");
+            UserRegistrationValidation validate = new UserRegistrationValidation();
+            Console.WriteLine("Enter First Name : ");
+            string firstName = Console.ReadLine();
+            bool fNameResult = validate.ValidateFirstName(firstName);
+            validate.PrintResult(fNameResult);
+        }
+    }
+}
---
+++ UserRegistrationProblem/UserRegistrationValidation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class UserRegistrationValidation
+    {
+        private static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        public bool ValidateFirstName(string firstName)
+        {
+            return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        }
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid First Name");
+            }
+            else
+            {
+                Console.WriteLine("Invalid First Name");
+            }
+        }
+    }
+}
+
