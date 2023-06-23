67a7030689e807df6402d7af43ee7fb86043a403
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 22:33:26 +0530

    [PALLAVI] Add: PasswordRule4

--- UserRegistrationProblem/UserRegistrationValidation.cs
+++ UserRegistrationProblem/UserRegistrationValidation.cs
@@ -11,7 +11,9 @@ namespace UserRegistrationProblem
     {
         // private static string REGEX_PASSWORD = "^[a-zA-Z0-9-+_!@#$%^&*.,?]{8,}$"; //regex for password Rule1-At least 8 character
         //  private static string REGEX_PASSWORD = "^(?=.*[A-Z])[A-Za-z0-9!@#$%^&*]{8,}$"; //regex for password Rule2- should contains at least one uppar case
-        private static string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9-+_!@#$%^&*.,?]{8,}$"; //passwordRule3-Should contain At least one numeric character
+        //private static string REGEX_PASSWORD = "^(?=.*[A-Z])(?=.*[0-9])[A-Za-z0-9-+_!@#$%^&*.,?]{8,}$"; //passwordRule3-Should contain At least one numeric character
+        private static string REGEX_PASSWORD = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%&*+-.]*[!@#$%&*+-.][^!@#$%&*+-.]*$)[\\S]{8,}$"; //passwordRule4-Exact one special Character
+
         public bool ValidatePassword(string password)
         {
             return Regex.IsMatch(password, REGEX_PASSWORD);
