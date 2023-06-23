d2b027820815edab9fd96f7a8b1895ebbdf6d789
“pallaviathawale” <athawalepallavi1998@gmai.com>
2023-06-23 22:09:14 +0530

    [PALLAVI]Add: ValidateEmail

--- UserRegistrationProblem/Program.cs
+++ UserRegistrationProblem/Program.cs
@@ -4,12 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("User Registration Problem");
             UserRegistrationValidation validate = new UserRegistrationValidation();
-            Console.WriteLine("Enter Last Name : ");
-            string lastName = Console.ReadLine();
-            bool lNameResult = validate.ValidateLastName(lastName);
-            validate.PrintResult(lNameResult);
+            //Console.WriteLine("Enter First Name : ");
+            //string firstName = Console.ReadLine();
+            //bool fNameResult = validate.ValidateFirstName(firstName);
+            //validate.PrintResult(fNameResult);
+            //Console.WriteLine("Enter Last Name : ");
+            //string lastName = Console.ReadLine();
+            //bool lNameResult = validate.ValidateLastName(lastName);
+            //validate.PrintResult(lNameResult);
+            Console.WriteLine("Enter Email Id : ");
+            string email = Console.ReadLine();
+            bool emailResult = validate.ValidateEmail(email);
+            validate.PrintResult(emailResult);
         }
     }
+    
 }
--- UserRegistrationProblem/UserRegistrationValidation.cs
+++ UserRegistrationProblem/UserRegistrationValidation.cs
@@ -9,20 +9,30 @@ namespace UserRegistrationProblem
 {
     public class UserRegistrationValidation
     {
-        private static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
-        public bool ValidateLastName(string lastName)
+        //private static string REGEX_FIRSTNAME = "^[A-Z][a-z]{2,}$";
+        //private static string REGEX_LASTNAME = "^[A-Z][a-z]{2,}$";
+        private static string REGEX_EMAIL = "^[a-zA-Z0-9]+([.][A-Za-z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]+)?$";
+        //public bool ValidateFirstName(string firstName)
+        //{
+        //    return Regex.IsMatch(firstName, REGEX_FIRSTNAME);
+        //}
+        //public bool ValidateLastName(string lastName)
+        //{
+        //    return Regex.IsMatch(lastName, REGEX_LASTNAME);
+        //}
+        public bool ValidateEmail(string email)
         {
-            return Regex.IsMatch(lastName, REGEX_LASTNAME);
+            return Regex.IsMatch(email, REGEX_EMAIL);
         }
         public void PrintResult(bool result)
         {
             if (result)
             {
-                Console.WriteLine("Valid Last Name");
+                Console.WriteLine("Valid.");
             }
             else
             {
-                Console.WriteLine("Invalid First Name");
+                Console.WriteLine("Invalid.");
             }
         }
     }
